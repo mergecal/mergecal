b58a8bff81307c59f35b436505a7be549533fe1b
Abe <abe@habet.dev>
2022-11-13 20:07:10 -0500

    Add config/urls

--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,7 @@ urlpatterns = [
     path("users/", include("calmerge.users.urls", namespace="users")),
     path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
+    path("calendars/", include("calmerge.calendars.urls", namespace="calendars")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
