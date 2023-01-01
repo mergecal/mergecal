e7ee994aea7e39b8db82562607b7c422a84a0df0
Abe <abe@habet.dev>
2023-01-01 08:44:18 -0500

    redirect to calendars upon signin

--- calmerge/users/views.py
+++ calmerge/users/views.py
@@ -42,7 +42,7 @@ class UserRedirectView(LoginRequiredMixin, RedirectView):
     permanent = False
 
     def get_redirect_url(self):
-        return reverse("users:detail", kwargs={"username": self.request.user.username})
+        return reverse("calendars:manage-calendar")
 
 
 user_redirect_view = UserRedirectView.as_view()
