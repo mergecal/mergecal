816d7a367712d0f217b8951963e4fc3caaec8cac
Abe <abe@habet.dev>
2023-01-02 22:04:02 -0500

    update test user redirect to /calendars/

--- calmerge/users/tests/test_views.py
+++ calmerge/users/tests/test_views.py
@@ -75,7 +75,7 @@ class TestUserRedirectView:
 
         view.request = request
 
-        assert view.get_redirect_url() == f"/users/{user.username}/"
+        assert view.get_redirect_url() == "/calendars/"
 
 
 class TestUserDetailView:
