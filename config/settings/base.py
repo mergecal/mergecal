ed219988dcd2befed0840166746d21babe4aacd0
Abe <abe@habet.dev>
2023-11-28 23:04:40 -0500

    linter

--- config/settings/base.py
+++ config/settings/base.py
@@ -32,7 +32,6 @@ SITE_ID = 1
 # https://docs.djangoproject.com/en/dev/ref/settings/#use-i18n
 USE_I18N = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#use-l10n
-USE_L10N = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#use-tz
 USE_TZ = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#locale-paths
