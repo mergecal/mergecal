15763605ead6936a243c068e404bce7ce993c591
Abe <abe@habet.dev>
2023-11-28 22:11:40 -0500

    update admin

--- mergecal/calendars/admin.py
+++ mergecal/calendars/admin.py
@@ -7,11 +7,35 @@ class CommentInline(admin.TabularInline):
     model = Source
 
 
+@admin.register(Calendar)
 class CalendarAdmin(admin.ModelAdmin):
-    inlines = [
-        CommentInline,
-    ]
+    list_display = ("name", "owner", "timezone", "uuid")  # Adjust the fields as needed
+    search_fields = [
+        "name",
+        "owner__username",
+    ]  # Enable search by name and owner's username
+    inlines = [CommentInline]
+    fieldsets = (
+        (None, {"fields": ("name", "uuid", "owner", "timezone")}),
+        (
+            "Advanced options",
+            {
+                "classes": ("collapse",),
+                "fields": ("calendar_file_str",),
+            },
+        ),
+    )
+    readonly_fields = ("uuid",)  # UUID should be readonly
 
 
-admin.site.register(Calendar, CalendarAdmin)
-admin.site.register(Source)
+@admin.register(Source)
+class SourceAdmin(admin.ModelAdmin):
+    list_display = ("name", "url", "calendar")  # Adjust the fields as needed
+    search_fields = [
+        "name",
+        "calendar__name",
+    ]  # Enable search by name and calendar name
+    list_filter = ("calendar",)  # Filter by calendar
+
+    # If you want to customize form fields
+    fieldsets = ((None, {"fields": ("name", "url", "calendar")}),)
