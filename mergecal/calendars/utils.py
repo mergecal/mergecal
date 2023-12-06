d305183bde9ad9ee87dfe88f83fec4e50ac79ff0
Abe <abe@habet.dev>
2023-12-06 12:50:34 -0500

    dont fail if no calendar founf

--- mergecal/calendars/utils.py
+++ mergecal/calendars/utils.py
@@ -19,18 +19,11 @@ def combine_calendar(calendar_instance):
 
     for source in calendar_instance.calendarOf.all():
         try:
-            r = requests.get(source.url)
-            r.raise_for_status()
-            cal = Calendar.from_ical(r.text)
-            # every part of the file...
-            for component in cal.subcomponents:
-                if component.name == "VEVENT":
-                    # ...which name is VEVENT will be added to the new file
-                    newcal.add_component(component)
-        except requests.exceptions.HTTPError as err:
-            logger.error(f"Error fetching URL {source.url}: {err}")
+            cal_data = fetch_calendar_data(source.url)
+            if cal_data:
+                process_calendar_data(cal_data, newcal)
         except Exception as err:
-            logger.error(f"Error parsing URL {source.url}: {err}")
+            logger.error(f"Unexpected error with URL {source.url}: {err}")
 
     cal_bye_str = newcal.to_ical()
     calendar_instance.calendar_file_str = cal_bye_str.decode("utf8")
@@ -38,3 +31,23 @@ def combine_calendar(calendar_instance):
     logger.info(
         f"Calendar for instance {calendar_instance.name} ({calendar_instance.uuid}) combined and saved."
     )
+
+
+def fetch_calendar_data(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return Calendar.from_ical(response.text)
+    except requests.exceptions.HTTPError as err:
+        logger.error(f"HTTP error fetching URL {url}: {err}")
+    except ValueError as err:
+        logger.error(f"Value error parsing URL {url}: {err}")
+    except Exception as err:
+        logger.error(f"Unexpected error fetching URL {url}: {err}")
+    return None
+
+
+def process_calendar_data(cal, newcal):
+    for component in cal.subcomponents:
+        if component.name == "VEVENT":
+            newcal.add_component(component)
