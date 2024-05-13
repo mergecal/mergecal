4dec93b3a909c58bf97248965e03a209ab6d3da8
Abe <abe@habet.dev>
2024-05-12 23:09:11 -0400

    use old domain

--- config/settings/production.py
+++ config/settings/production.py
@@ -111,7 +111,7 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-from-email
 DEFAULT_FROM_EMAIL = env(
     "DJANGO_DEFAULT_FROM_EMAIL",
-    default="MergeCal <noreply@Merge-Cal.com>",
+    default="MergeCal <noreply@mergecal.habet.dev>",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#server-email
 SERVER_EMAIL = env("DJANGO_SERVER_EMAIL", default=DEFAULT_FROM_EMAIL)
