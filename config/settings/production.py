6675f416deb6ff8a9da6442895cf9af2de51e501
Abe Hanoka <abe@habet.dev>
2024-06-21 08:53:53 -0400

    update to new domain mergecal.org

--- config/settings/production.py
+++ config/settings/production.py
@@ -15,7 +15,7 @@ from .base import DATABASES, INSTALLED_APPS, env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["Merge-Cal.com"])
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["Mergecal.org"])
 
 # DATABASES
 # ------------------------------------------------------------------------------
--- mergecal/contrib/sites/migrations/0003_set_site_domain_and_name.py
+++ mergecal/contrib/sites/migrations/0003_set_site_domain_and_name.py
@@ -40,7 +40,7 @@ def update_site_forward(apps, schema_editor):
     _update_or_create_site_with_sequence(
         Site,
         schema_editor.connection,
-        "Merge-Cal.com",
+        "Mergecal.org",
         "MergeCal",
     )
 
