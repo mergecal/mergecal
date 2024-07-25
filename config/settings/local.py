092946fe6dd1db6fbda9a64af365951c355d7682
Abe Hanoka <abe@habet.dev>
2024-07-24 23:15:45 -0400

    add kolo.app to local env

--- config/settings/local.py
+++ config/settings/local.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = ["whitenoise.runserver_nostatic", *INSTALLED_APPS]
 # ------------------------------------------------------------------------------
 # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#prerequisites
 INSTALLED_APPS += ["debug_toolbar"]
+# MIDDLEWARE = ["kolo.middleware.KoloMiddleware", *MIDDLEWARE]
 # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#middleware
 MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]
 # https://django-debug-toolbar.readthedocs.io/en/latest/configuration.html#debug-toolbar-config
