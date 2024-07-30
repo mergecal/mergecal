915624d7624e9c161a61e157dde5f04aef210f8d
Abe Hanoka <abe@habet.dev>
2024-07-30 14:14:52 -0400

    fix test by mocking djstripe method

--- mergecal/billing/apps.py
+++ mergecal/billing/apps.py
@@ -7,4 +7,4 @@ class BillingConfig(AppConfig):
     verbose_name = "Billing"
 
     def ready(self):
-        import billing.signals  # noqa: F401
+        import mergecal.billing.signals  # noqa: F401
