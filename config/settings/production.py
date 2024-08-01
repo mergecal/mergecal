3e68078178f51e73a6b183f4673fc48be5cb8b10
Abe Hanoka <abe@habet.dev>
2024-08-01 14:23:44 -0400

    rm django-storages

--- config/settings/production.py
+++ config/settings/production.py
@@ -68,7 +68,7 @@ SECURE_CONTENT_TYPE_NOSNIFF = env.bool(
 # STORAGES
 # ------------------------------------------------------------------------------
 # https://django-storages.readthedocs.io/en/latest/#installation
-INSTALLED_APPS += ["storages"]
+# INSTALLED_APPS += ["storages"]
 # https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
 AWS_ACCESS_KEY_ID = env("DJANGO_AWS_ACCESS_KEY_ID")
 # https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
