66c8f9e66ac0a5e6b07b01e4c5de8853d36e99e6
Abe Hanoka <abe@habet.dev>
2024-08-06 20:09:20 -0400

    add 4 email paragraph

--- mergecal/core/emails.py
+++ mergecal/core/emails.py
@@ -1,6 +1,5 @@
 # ruff: noqa: PLR0913, FBT002, FBT001
 # your_app/emails.py
-
 from django.core.mail import EmailMessage
 
 from mergecal.core.constants import MailjetTemplates
@@ -43,5 +42,60 @@ class TemplateEmailMessage(EmailMessage):
         self.merge_data = {
             recipient: self.context.get(recipient, {}) for recipient in self.to
         }
+        # Send the email
+        return super().send(fail_silently=fail_silently)
+
+
+FOUR = 4
+
+
+class MultiBodyTemplateEmailMessage(EmailMessage):
+    DEFAULT_TEMPLATE_ID = MailjetTemplates.BASE
+
+    def __init__(
+        self,
+        subject: str,
+        to_users: list[User],
+        bodies: list[str],
+        from_email: str | None = None,
+        template_id: str | None = None,
+    ):
+        # Extract email addresses from the user queryset
+        to_emails = [user.email for user in to_users]
+
+        # Initialize the parent class with the necessary parameters
+        super().__init__(
+            subject=subject,
+            body="",
+            from_email=from_email,
+            to=to_emails,
+        )
+
+        # Set the template ID based on the number of bodies
+        if template_id:
+            self.template_id = template_id
+        elif len(bodies) == FOUR:
+            self.template_id = MailjetTemplates.FOUR_PARAGRAPHS
+        else:
+            self.template_id = self.DEFAULT_TEMPLATE_ID
+
+        # Set the context for each recipient
+        self.context = {user.email: {"name": user.username} for user in to_users}
+
+        # Add body fields to context based on the number of bodies
+        for i, body in enumerate(bodies, start=1):
+            for email in self.context:
+                self.context[email][f"body{i}"] = body
+
+    def send(self, fail_silently: bool = False) -> int:
+        if not isinstance(self.context, dict):
+            msg = "Context must be a dictionary with recipient emails as keys."
+            raise TypeError(msg)
+
+        # Set merge_data for each recipient
+        self.merge_data = {
+            recipient: self.context.get(recipient, {}) for recipient in self.to
+        }
+
         # Send the email
         return super().send(fail_silently=fail_silently)
