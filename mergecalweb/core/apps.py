61f1e550dbc2795e926dff577158fdbe5ccb1aac
Abe Hanoka <abe@habet.dev>
2025-11-06 00:05:44 -0500

    add posthog

--- mergecalweb/core/apps.py
+++ mergecalweb/core/apps.py
@@ -1,6 +1,16 @@
+import os
+
 from django.apps import AppConfig
+from django.conf import settings
 
 
 class CoreConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "mergecalweb.core"
+
+    def ready(self):
+        if os.environ.get("DJANGO_SETTINGS_MODULE") == "config.settings.production":
+            import posthog  # noqa: PLC0415
+
+            posthog.api_key = settings.POSTHOG_API_KEY
+            posthog.host = "https://m.mergecal.org"
